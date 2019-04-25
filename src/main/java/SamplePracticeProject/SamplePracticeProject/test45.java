5860fef4eb01c631a195221e0e827c557ed0e837
ab76258 <ab76258@5CG6332Q2P.ctl.intranet>
2019-04-25 23:33:23 +0530

    second commit

---
+++ src/main/java/SamplePracticeProject/SamplePracticeProject/test45.java
@@ -0,0 +1,12 @@
+package SamplePracticeProject.SamplePracticeProject;
+
+public class test45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HEllo WORLD");
+
+	}
+
+}
